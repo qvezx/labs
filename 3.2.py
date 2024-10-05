dec9a5431a2dfc6884dd052932b277792893420f
qvezx <qvezx@yandex.ru>
2024-10-05 14:59:17 +0300

    correct

--- 3.2.py
+++ 3.2.py
@@ -4,4 +4,5 @@ def user_input():
     with open ("input.txt", "a") as file:
         file.write(text)
 
+print("Введите текст который хотите добавить в файл") 
 user_input()
--- 3.3.py
+++ 3.3.py
@@ -1,17 +1,27 @@
 def read(var):
-    try:
-        if var == 1:
-            with open ("example.txt") as example:
-                content = example.read()
-                print(content)
+  try:
+    if var == 1:
+      with open ("example.txt") as example:
+        content = example.read()
+        return content
 
-        elif var == 2:
-            result = ""
-            with open ("example.txt") as example:
-                for line in example:
-                    result += line
+    elif var == 2:
+      result = ""
+      with open ("example.txt") as example:
+        for line in example:
+          result += line
+        return result
 
-    except FileNotFoundError:
-        print("Такого файла не существует")
+  except FileNotFoundError:
+    print("Такого файла не существует")
 
-print(read(1))
+a = int(input("Выбирете каким способом хотите открыть файл(цифра 1 - первый способ, цифра 2 - второй способ): "))
+
+if (a == 1):
+  print(read(1))
+
+elif (a == 2):
+  print(read(2))
+
+else:
+  print("такого варианта не существует")
